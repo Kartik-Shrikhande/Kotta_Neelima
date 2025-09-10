01a7c7eb1bc5670b2cc375d32aa7f75e16610d0e
KARTIK1111 <kartikshrikhande11@gmail.com>
2025-09-10 14:57:20 +0530

    added authentication and authorization

--- src/routes/ruralDistressRoutes.js
+++ src/routes/ruralDistressRoutes.js
@@ -6,12 +6,15 @@ const {
   updateRuralDistress,
   deleteRuralDistress
 } = require('../controllers/ruralDistressController');
+const authenticationMiddleware = require('../middleware/authenticationMiddleware');
 
 const router = express.Router();
 
-// Routes for Rural Distress
-router.post('/create', createRuralDistress);
 router.get('/all', getAllRuralDistress);
+
+// Routes for Rural Distress
+router.use(authenticationMiddleware.authenticateUser,authenticationMiddleware.authorizeRoles(['admin']))
+router.post('/create', createRuralDistress);
 // router.get('/:id', getRuralDistressById);
 router.put('/:id', updateRuralDistress);
 router.delete('/:id', deleteRuralDistress);
